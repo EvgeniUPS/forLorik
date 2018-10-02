fd53edbd95101a7792d58116223b6ba185aaeca6
EvgeniUPS <evgeniups@gmail.com>
2018-10-02 18:44:36 +0300

    edit path
    Signed-off-by: EvgeniUPS <evgeniups@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 var numbers = [
-  "/img/00.png",
-  "/img/01.png",
-  "/img/02.png",
-  "/img/03.png",
-  "/img/04.png",
-  "/img/05.png",
-  "/img/06.png",
-  "/img/07.png",
-  "/img/08.png",
-  "/img/09.png"
+  "./img/00.png",
+  "./img/01.png",
+  "./img/02.png",
+  "./img/03.png",
+  "./img/04.png",
+  "./img/05.png",
+  "./img/06.png",
+  "./img/07.png",
+  "./img/08.png",
+  "./img/09.png"
 ];
 
 let num1 = document.getElementsByClassName("num1");
